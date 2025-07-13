3f4782edbfd1a3450b12be524178ac425651d9b1
AccountZero <hubert.korona@gmail.com>
2025-07-13 17:22:45 +0200

    complete rewrite since the realisation of github actions

--- config/initializers/assets.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.assets.version = '1.0'
-
-# Optional: Add additional asset paths
-# Rails.application.config.assets.paths << Rails.root.join("app/assets/fonts")
-
-# Precompile additional assets.
-Rails.application.config.assets.precompile += %w( application.css application.js )
